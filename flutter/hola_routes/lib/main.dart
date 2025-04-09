b9c266375853fbf03ad973a6f93061b348e93897
Chihau Chau <chihau@gmail.com>
2025-04-09 14:12:21 -0400

    Agrega ejemplo 'Hola Routes'

---
+++ flutter/hola_routes/lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          useMaterial3: false,
+          primarySwatch: Colors.blue,
+        ),
+        home: Screen1());
+  }
+}
+
+class Screen1 extends StatelessWidget {
+  Screen1({super.key});
+
+  var myController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hola Routes - Screen 1'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Ingresa tu nombre',
+                ),
+                style: TextStyle(fontSize: 25),
+                textAlign: TextAlign.center,
+                controller: myController,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Screen2(),
+                      settings: RouteSettings(arguments: myController.text),
+                    ),
+                  );
+                },
+                child: Text('Ir a Screen2'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Screen2 extends StatelessWidget {
+  const Screen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String? nombre = ModalRoute.of(context)?.settings.arguments as String;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hola Routes - Screen 2'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Hola $nombre',
+                style: TextStyle(fontSize: 25),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Volver'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
